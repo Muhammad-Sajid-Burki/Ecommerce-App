490414c9e0c164f2601b5ee9ee0f21dfe4fadd8c
Muhammad Sajid <sk6011525@gamil.com>
2021-06-21 20:22:32 +0500

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Codepeur";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days Days of flutter $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
